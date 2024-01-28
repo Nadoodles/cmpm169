c0218aa6eca33b420a2792ed1a6449028c3dfcd8
Jose <nadalesjoseluis@gmail.com>
2024-01-27 22:52:10 -0800

    added stars

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -6,11 +6,13 @@ const VALUE2 = 2;
 let myInstance;
 let canvasContainer;
 let circles = [];
-let fireworks = []; // Renamed 'nodes' to 'fireworks'
+let fireworks = [];
 let gravity;
 let nodeCount = 200;
-let animationDuration = 60; // Number of frames for the animation
+let animationDuration = 60;
 let clickX, clickY;
+let waves = [];
+let stars = [];
 
 class MyClass {
   constructor(param1, param2) {
@@ -23,7 +25,7 @@ class MyClass {
   }
 }
 
-class Firework extends p5.Vector { // Renamed 'Node' to 'Firework'
+class Firework extends p5.Vector {
   constructor(x, y, color) {
     super(x, y);
     this.vel = p5.Vector.random2D().mult(random(2, 5));
@@ -36,7 +38,7 @@ class Firework extends p5.Vector { // Renamed 'Node' to 'Firework'
     this.add(this.vel);
   }
 
-  attractFireworks(otherFireworks) { // Renamed 'attractNodes' to 'attractFireworks'
+  attractFireworks(otherFireworks) {
     for (let otherFirework of otherFireworks) {
       if (otherFirework !== this) {
         let force = p5.Vector.sub(otherFirework, this);
@@ -51,7 +53,45 @@ class Firework extends p5.Vector { // Renamed 'Node' to 'Firework'
   }
 }
 
-// setup() function is called once when the program starts
+class Wave {
+  constructor() {
+    this.amplitude = random(10, 100);
+    this.period = random(100, 300);
+    this.yOffset = random(TWO_PI);
+  }
+
+  update() {
+    this.yOffset += 0.01;
+  }
+
+  display() {
+    fill(0, 0, 255);
+    noStroke();
+    beginShape();
+    vertex(0, height);
+    for (let x = 0; x <= width; x += 20) {
+      let y = this.amplitude * sin(TWO_PI * x / this.period + this.yOffset);
+      vertex(x, height - 50 + y);
+    }
+    vertex(width, height);
+    endShape(CLOSE);
+  }
+}
+
+class Star {
+  constructor() {
+    this.x = random(width);
+    this.y = random(height);
+    this.size = random(1, 3);
+  }
+
+  display() {
+    fill(255);
+    noStroke();
+    ellipse(this.x, this.y, this.size, this.size);
+  }
+}
+
 function setup() {
   let canvasWidth = 1300;
   let canvasHeight = 1000;
@@ -62,7 +102,6 @@ function setup() {
   gravity = createVector(0, 0.2);
 
   $(window).resize(function () {
-    console.log("Resizing...");
     resizeCanvas(canvasContainer.width(), canvasContainer.height());
   });
 
@@ -72,7 +111,6 @@ function setup() {
   var centerVert = windowHeight / 2;
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
   background(0);
 
@@ -87,7 +125,6 @@ function draw() {
 
     // Remove the circle if its lifespan is zero
     if (circle.lifespan <= 0) {
-      fireworks.push(new Firework(circle.x, circle.y, circle.color)); // Create a firework when circle's lifespan ends
       circles.splice(i, 1);
     }
   }
@@ -95,8 +132,9 @@ function draw() {
   // Update and draw fireworks
   for (let i = fireworks.length - 1; i >= 0; i--) {
     let firework = fireworks[i];
-    firework.attractFireworks(fireworks); // Attract other fireworks to this firework
+    firework.attractFireworks(fireworks);
     firework.update();
+    fill(firework.color);
     ellipse(firework.x, firework.y, 10, 10);
 
     // Decrease the lifespan
@@ -108,25 +146,61 @@ function draw() {
     }
   }
 
+  // Draw waves
+  for (let i = 0; i < waves.length; i++) {
+    waves[i].update();
+    waves[i].display();
+  }
+
+  // Draw stars
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].display();
+  }
+
   // call a method on the instance
   myInstance.myMethod();
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
   // Create a new circle at the mouse position
   let circle = {
     x: mouseX,
     y: mouseY,
-    diameter: 50,
+    diameter: 30,
     color: color(random(255), random(255), random(255)),
-    lifespan: 100,
+    lifespan: 20,
   };
 
+  // Push the circle first
   circles.push(circle);
+
+  // Then create and push multiple fireworks
+  for (let i = 0; i < 15; i++) {
+    let firework = new Firework(
+      mouseX + random(-20, 20),
+      mouseY + random(-20, 20),
+      circle.color
+    );
+
+    fireworks.push(firework);
+  }
 
   // Set the click coordinates for the nodes animation
   clickX = mouseX;
   clickY = mouseY;
   animationDuration = 60; // Reset animation duration
+}
+
+function keyPressed() {
+  if (key === 'W' || key === 'w') {
+    waves.push(new Wave());
+  }
+
+  if (key === "E" || key === "e") {
+    waves = [];
+  }
+
+  if (key === 'S' || key === 's') {
+    stars.push(new Star());
+  }
 }
