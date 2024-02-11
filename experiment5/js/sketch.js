120a4a8df47421c3eeb70f9709b38ce450907560
Jose <nadalesjoseluis@gmail.com>
2024-02-10 17:13:55 -0800

    initial push

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -25,41 +25,63 @@ class MyClass {
     }
 }
 
+let backgroundImage;
+
+function preload() {
+    // Load the Blender model (replace 'model.obj' with the path to your model file)
+    myModel = loadModel('Sheep.obj', true);
+    textureImg = loadImage('sheep_texture.png');
+    backgroundImage = loadImage('grass.jpeg');
+  }
+
+
 // setup() function is called once when the program starts
 function setup() {
     // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    let canvas = createCanvas(canvasContainer.width(), 1000, WEBGL);
     canvas.parent("canvas-container");
     // resize canvas is the page is resized
     $(window).resize(function() {
         console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
+        resizeCanvas(canvasContainer.width(), 1000, WEBGL);
     });
     // create an instance of the class
     myInstance = new MyClass(VALUE1, VALUE2);
 
     var centerHorz = windowWidth / 2;
     var centerVert = windowHeight / 2;
+
+
+
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+    background(0); // Black background color
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    image(backgroundImage, -825, -500, canvasContainer.width(), 1000);
+
+    // Apply the texture
+    texture(textureImg);
+
+    // Rotate the model upright
+    rotateX(PI); // Rotate around X-axis by 180 degrees to make it upright
+    rotateY(HALF_PI); // Rotate around Y-axis by 90 degrees
+
+    // Render the loaded model
+    scale(1); // Scale the model
+    model(myModel);
+
+    // Draw green rectangle at the bottom
+    fill(0, 255, 0); // Green fill color
+    noStroke(); // No outline
+    rectMode(CENTER);
+    let rectHeight = 50; // Adjust height as needed
+    rect(width / 2, height - rectHeight / 2, width, rectHeight); // Draw the rectangle at the bottom
 }
+
+
 
 // mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
