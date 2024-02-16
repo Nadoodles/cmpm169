f1e4e8f5a7225490b0101ee0f7db8c1f7e738f8c
Jose <nadalesjoseluis@gmail.com>
2024-02-16 14:43:29 -0800

    we'll see what happens

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,6 +1,6 @@
 // sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Jose Nadales
+// Date: 2/162/24
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
@@ -24,41 +24,114 @@ class MyClass {
         // code to run when method is called
     }
 }
+let inputText = 'I am going to use this termporarily';
+
+let fontSizeMax = 20;
+let fontSizeMin = 10;
+let spacing = 12; // line height
+let kerning = 0.5; // between letters
+
+let fontSizeStatic = false;
+let blackAndWhite = false;
+
+function preload() {
+    // Load your .webp file
+    img = loadImage('gojo.webp');
+  }
+  
 
 // setup() function is called once when the program starts
 function setup() {
     // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    let canvas = createCanvas(canvasContainer.width(), 1500);
     canvas.parent("canvas-container");
     // resize canvas is the page is resized
     $(window).resize(function() {
         console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
+        resizeCanvas(canvasContainer.width(), 1500);
     });
     // create an instance of the class
     myInstance = new MyClass(VALUE1, VALUE2);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    textFont('Times');
+    textSize(10);
+    textAlign(LEFT, CENTER);
+    print(img.width + ' • ' + img.height);
+
+
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+    background(0);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    var x = 0;
+    var y = 10;
+    var counter = 0;
+
+    while (y < height) {
+        // translate position (display) to position (image)
+        img.loadPixels();
+        // get current color
+        var imgX = round(map(x, 0, width, 0, img.width));
+        var imgY = round(map(y, 0, height, 0, img.height));
+        var c = color(img.get(imgX, imgY));
+        var greyscale = round(red(c) * 0.222 + green(c) * 0.707 + blue(c) * 0.071);
+
+        push();
+        translate(x, y);
+
+        if (fontSizeStatic) {
+        textSize(fontSizeMax);
+        if (blackAndWhite) {
+            fill(greyscale);
+        } else {
+            fill(c);
+        }
+        } else {
+        // greyscale to fontsize
+        var fontSize = map(greyscale, 0, 255, fontSizeMax, fontSizeMin);
+        fontSize = max(fontSize, 1);
+        textSize(fontSize);
+        if (blackAndWhite) {
+            fill(0);
+        } else {
+            fill(c);
+        }
+        }
+
+        var letter = inputText.charAt(counter);
+        text(letter, 0, 0);
+        var letterWidth = textWidth(letter) + kerning;
+        // for the next letter ... x + letter width
+        x += letterWidth;
+
+        pop();
+
+        // linebreaks
+        if (x + letterWidth >= width) {
+        x = 0;
+        y += spacing;
+        }
+
+        counter++;
+        if (counter >= inputText.length) {
+        counter = 0;
+        }
+    }
+    noLoop();
+}
+
+function keyReleased() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+  // change render mode
+  if (key == '1') fontSizeStatic = !fontSizeStatic;
+  // change color style
+  if (key == '2') blackAndWhite = !blackAndWhite;
+  print('fontSizeMin: ' + fontSizeMin + ', fontSizeMax: ' + fontSizeMax + ', fontSizeStatic: ' + fontSizeStatic + ', blackAndWhite: ' + blackAndWhite);
+  loop();
+
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
